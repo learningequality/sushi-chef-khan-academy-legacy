4118b60d006c0557b37c75e4e55361cad7d18a39
Aron Fyodor Asor <aronasorman@gmail.com>
2015-10-23 16:46:19 -0700

    logic for downloading translations.

--- contentpacks/contentpacks.py
+++ contentpacks/contentpacks.py
@@ -1,8 +1,11 @@
 
 import collections
+import fnmatch
 import os
-
-import requests
+import zipfile
+from babel.messages.catalog import Catalog
+from babel.messages.pofile import read_po
+from .utils import download_and_cache_file
 
 
 LangpackResources = collections.namedtuple(
@@ -42,7 +45,7 @@ def retrieve_language_resources(lang: str, version: str) -> LangpackResources:
     return LangpackResources(topic_data, content_data, exercise_data, subtitle_data, kalite_catalog, ka_catalog, dubbed_video_mapping)
 
 
-def retrieve_translations(crowdin_project_name, crowdin_secret_key, lang_code="en", includes=None) -> polib.POFile:
+def retrieve_translations(crowdin_project_name, crowdin_secret_key, lang_code="en", includes="*.po") -> Catalog:
 
     request_url_template = "https://api.crowdin.com/api/project/{project_id}/download/{lang_code}.zip?key={key}"
     request_url = request_url_template.format(
@@ -51,4 +54,23 @@ def retrieve_translations(crowdin_project_name, crowdin_secret_key, lang_code="e
         key=crowdin_secret_key,
     )
 
-    zip_path = download_and_cache_file(url)
+    zip_path = download_and_cache_file(request_url)
+
+    catalogs = []
+    with zipfile.ZipFile(zip_path) as zf:
+        filenames = fnmatch.filter(zf.namelist(), includes)
+        for filename in filenames:
+            f = zf.open(filename)
+            pofile = read_po(f)
+            catalogs.append(pofile)
+
+    return _combine_catalogs(*catalogs)
+
+
+def _combine_catalogs(*catalogs):
+    catalog = Catalog()
+
+    for oldcatalog in catalogs:
+        catalog._messages.update(oldcatalog._messages)
+
+    return catalog
