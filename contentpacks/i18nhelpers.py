f717ab82aa1c415b8d1a5fe45525a8cf3e230850
Aron Fyodor Asor <aronasorman@gmail.com>
2015-10-23 10:41:59 -0700

    move stuff to contentpacks.

--- contentpacks/i18nhelpers.py
+++ contentpacks/i18nhelpers.py
@@ -2,6 +2,8 @@
 import collections
 import os
 
+import requests
+
 
 LangpackResources = collections.namedtuple(
     "LangpackResources",
@@ -38,3 +40,30 @@ def retrieve_language_resources(lang: str, version: str) -> LangpackResources:
     dubbed_video_mapping = retrieve_dubbed_video_mapping(lang)
 
     return LangpackResources(topic_data, content_data, exercise_data, subtitle_data, kalite_catalog, ka_catalog, dubbed_video_mapping)
+
+
+def retrieve_translations(crowdin_project_name, crowdin_secret_key, lang_code="en", includes=None) -> polib.POFile:
+
+    request_url_template = "https://api.crowdin.com/api/project/{project_id}/download/{lang_code}.zip?key={key}"
+    request_url = request_url_template.format(
+        project_id=crowdin_project_name,
+        lang_code=lang_code,
+        key=crowdin_secret_key,
+    )
+
+    zip_path = download_and_cache_file(url)
+
+
+def download_and_cache_file(url, cachedir=None, ignorecache=False) -> str:
+    "Download the given url if it's not saved in cachedir. Returns the path to
+    the file. Always download the file if ignorecache is True.
+    "
+    if not cachedir:
+        cachedir = os.path.join(os.getcwd(), "build")
+
+    path = os.path.join(cachedir, os.path.basename(url))
+
+    if ignorecache or not os.path.exists(path):
+        urllib.urlretrieve(url, path)
+
+    return path
