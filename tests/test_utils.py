073e63e9dc2dd56f9b88d8b915f39c90b0a50070
Aron Fyodor Asor <aronasorman@gmail.com>
2015-10-23 11:51:12 -0700

    move fixtures to tests/fixtures

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,7 @@ from contentpacks.utils import download_and_cache_file
 
 class Test_download_and_cache_file:
 
-    @vcr.use_cassette("fixtures/cassettes/generic_request.yml")
+    @vcr.use_cassette("tests/fixtures/cassettes/generic_request.yml")
     def test_returns_existing_file(self):
         url = "https://google.com"
         path = download_and_cache_file(url)
