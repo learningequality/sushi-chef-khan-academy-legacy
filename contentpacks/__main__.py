baed494611bcc71655c77baca7eec3aad22e9798
Aron Fyodor Asor <aronasorman@gmail.com>
2016-02-16 09:29:26 -0800

    Reduce the logging level.
    So we don't blow up our logs.

--- contentpacks/__main__.py
+++ contentpacks/__main__.py
@@ -78,7 +78,7 @@ def main():
 
     log_file = args["--logging"] or "debug.log"
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     try:
         make_language_pack(lang, version, sublangs, out, no_assessment_items, no_subtitles)
