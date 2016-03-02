7af36d4bd1fedadcc5bda7d7abe7bb0b85ad1419
Aron Fyodor Asor <aronasorman@gmail.com>
2016-03-01 20:10:36 -0800

    When generating all_metadata, skip zip files without metadata.json.

--- collectmetadata.py
+++ collectmetadata.py
@@ -22,8 +22,11 @@ def read_metadata(filename: pathlib.Path) -> dict:
     with zipfile.ZipFile(str(filename)) as zf:
         # necessary, since zf.open isn't smart enough to auto-decode and return
         # str (it returns bytes)
-        s = zf.read(CONTENTPACK_METADATA_FILENAME).decode('utf-8') 
-        return json.loads(s)
+        try:
+            s = zf.read(CONTENTPACK_METADATA_FILENAME).decode('utf-8') 
+            return json.loads(s)
+        except KeyError:    # no metadata file, skip zipfile
+            return None
 
 
 def return_all_contentpack_files(dir: pathlib.Path) -> [pathlib.Path]:
@@ -46,6 +49,7 @@ def main():
     out.parent.mkdir(exist_ok=True, parents=True)
 
     all_metadata = [read_metadata(f) for f in return_all_contentpack_files(dir)]
+    all_metadata = [m for m in all_metadata if m]
     with open(str(out), "w") as f:
         json.dump(all_metadata, f)
 
